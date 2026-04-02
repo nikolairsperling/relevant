994826cbe5b9e0a55a1fb82429c3973ed6adb4f4
nikolairsperling <hello@kayuyimedia-marketing.com>
2026-04-02 11:34:25 +0200

    fix: typo Hro -> Hero in root page

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <Hro />
+      <Hero />
       <Problem />
       <HowItWorks />
       <ForWho />
